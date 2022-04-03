3295abc37be9c201199634d37856b4325cf25835
Kamsu10 <kamsudylane@gmail.com>
2022-04-03 22:13:14 +0200

    recent

---
+++ public/controllers/roleController.js
@@ -0,0 +1,14 @@
+const db= require('../models/index')
+
+module.exports = {
+    
+// create one role
+   async addRole(req,res){
+
+   },
+   
+// display all roles
+   async displayRole(req,res){
+
+   }
+}
