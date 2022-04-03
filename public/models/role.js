cff8559f2ab9ac1a210126ed1128e6dd7cb1b06c
Kamsu10 <kamsudylane@gmail.com>
2022-04-03 19:08:47 +0200

    latest

--- public/models/role.js
+++ public/models/role.js
@@ -3,36 +3,26 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Role extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.hasMany(models.Tache);
-      User.belongsTo(models.Role,{foreignKey:'RoleId',onDelete:'CASCADE',onUpdate:'CASCADE'});
+      Role.hasMany(models.User);
     }
   }
-  User.init({
-    id:{
+  Role.init({
+  id:{
     allowNull: false,
     autoIncrement: true,
     primaryKey: true,
     type: DataTypes.INTEGER.UNSIGNED
     },
-    confirmed:{
-      type: DataTypes.BOOLEAN, 
-      allowNull: false, 
-      defaultValue: false
-   },
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'Role',
   });
-  return User;
+  return Role;
 };
